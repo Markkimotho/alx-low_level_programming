b6100e2f2ef7e268385bb3f0f23750388edddc39
MarkTinega <martinozas@gmail.com>
2022-09-08 06:58:11 -0700

    5-printf.c and 6-size.c corrections to betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * the main function has an integer return value;returning 0
- * accepts void arguments
+/**
+ *main - entry point of all our code 
+ *Return:0
  */
 int main(void)
 {
