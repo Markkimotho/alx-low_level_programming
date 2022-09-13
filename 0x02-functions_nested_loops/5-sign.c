91abc8185ccbd88e523de60d3966e0048bec6f55
MarkTinega <martinozas@gmail.com>
2022-09-13 05:22:49 -0700

    5-sign.c soln

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  *print_sign - outputs the sign on an integer
+  *@n: integer input
+  *Return: 0 if int is 0, 1 if +ve, -1 if -ve
+  *
+  */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
