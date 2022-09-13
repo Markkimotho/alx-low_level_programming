d16a8d846a00e0e8d4f898e8358484c8aff3a677
MarkTinega <martinozas@gmail.com>
2022-09-13 09:48:56 -0700

    11-print_to_98.c soln

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  *print_to_98 - prints all the numbers to 98 inclusive
+  *@n: accepts integers as parameters
+  *Return: doesnt care about return type
+  *
+  */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ";
+			}
+			n--;
+		}
+
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
