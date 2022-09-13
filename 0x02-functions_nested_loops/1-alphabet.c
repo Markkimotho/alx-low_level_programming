202f36d8a02a7be1a72587ded8f7124cd1fd56a3
MarkTinega <martinozas@gmail.com>
2022-09-13 03:40:27 -0700

    1-alphabet.c modif

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- *main - entry point
+ *print_alphabet - custom function
  *Description: Prints out the alphabet letter in lowercase in one line followed by a newline
- *Return: 0 on successful execution
+ *Return: void meaning the return type does not matter
  */
-int main(void)
-{
-	print_alphabet();
-}
-
 void print_alphabet(void)
 {
 	char ch;
+
 	for(ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
