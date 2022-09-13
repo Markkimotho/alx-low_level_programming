1928a41559d91d5c8230a3c3cebced49216439a5
MarkTinega <martinozas@gmail.com>
2022-09-13 06:19:04 -0700

    7-print_last_digit.c modif

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,11 +12,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		m = (n % 10) * (-1);
+		_putchar(m + '0');
 		return (m);
 	}
 	else
 	{
 		m = n % 10;
+		_putchar(m + '0');
 		return (m);
 	}
 }
