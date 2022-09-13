477836e8a2e5613821287e8d1a746e3f0cb1d826
MarkTinega <martinozas@gmail.com>
2022-09-13 05:42:13 -0700

    6-abs.c soln

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *_abs - finds an absolute integer value
+  *@int: accepts integer values
+  *Return: 0 for successful execution
+  *
+  */
+
+int _abs(int)
+{
+	int n;
+	int m;
+
+	if (n < 0)
+	{
+		m = n * (-1);
+		_putchar(m);
+		return (0);
+	}
+	else
+	{
+		_putchar(n);
+		return (0);
+	}
+}
