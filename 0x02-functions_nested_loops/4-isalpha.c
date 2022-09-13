373c20c8529045488f214ddc43fc531b0962059e
MarkTinega <martinozas@gmail.com>
2022-09-13 04:54:57 -0700

    4-isalpha.c soln

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#inclue "main.h"
+
+/**
+ * _isalpha - identifies the alphabetic character
+ * @c: accepts any ascii character as a parameter
+ * Return: 0 if not an alphabetic character and 1 if alphabetic
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 && c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
