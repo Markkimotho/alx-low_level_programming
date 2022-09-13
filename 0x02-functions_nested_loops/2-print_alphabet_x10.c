f1d8ef3c96198740c50291e4f39a7f2a8d2a1b3c
MarkTinega <martinozas@gmail.com>
2022-09-13 03:51:37 -0700

    1-alphabet.c solnn

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
+ *print_alphabet_x10 - prints the alphabet letters 10 times
  *
- *
- *
+ *Return: void which means it excludes the return value
  */
 
 void print_alphabet_x10(void)
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 'a'; ch <='z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
