e06adc1384e613ccd58820d2d2d144d8767b012b
MarkTinega <martinozas@gmail.com>
2022-09-13 03:02:55 -0700

    Adding header files and correcting the _putchar code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 /**
  * main - entry point
  * Return: 0 for success
  */
 int main(void)
 {
-	puts("_putchar");
+	char pc[] = "_putchar";
+
+	for (int i = 0; pc[i] != '\0'; i++)
+	{
+		_putchar(pc[i]);
+	}
 	return (0);
 }
