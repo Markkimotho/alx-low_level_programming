271d94b1cf85ca090c8d79809354f11a96990e88
MarkTinega <martinozas@gmail.com>
2022-09-13 07:23:49 -0700

    10-add.c soln

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *add - sums up two integers
+  *@n: first integer
+  *@m: second integer
+  *Return: returns the result after the sum
+  *
+  */
+
+int add(int n, int m)
+{
+	int p;
+
+	p = n + m;
+	return (p);
+}
