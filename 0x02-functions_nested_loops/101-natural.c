bd9a0f6f9dd32052c5b08fbe8dc5c752ccfb1fc7
MarkTinega <martinozas@gmail.com>
2022-09-14 14:18:38 -0700

    101-natural.c soln

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,16 @@
 int main(void)
 {
 	int i;
+	unsigned int j = 0;
 
 	for (i = 1 ; i < 1024 ; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("%i", i);
+			j = j + i;
 		}
 	}
+	printf("%i", j);
 	printf("\n");
 
 	return (0);
