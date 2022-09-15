8f961cec2c682de0c7fd1eab6eb6eda1f188705e
MarkTinega <martinozas@gmail.com>
2022-09-15 02:59:20 -0700

    1-digit. solution

---
+++ 0x04-more_functions_nested_loops/1-digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *_isdigit - checks whether a character is an digit or not
+  *@c: accepts any character as parameter
+  *Return: 0 if not a digit, 1 if a digit
+  */
+
+int _isdigit(int c)
+{
+	if (c >= 48 || c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
