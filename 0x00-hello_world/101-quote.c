d88262f2deb5c2c500281c52d42745a42fa6608b
MarkTinega <martinozas@gmail.com>
2022-09-15 03:58:35 -0700

    general changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - entry point to the program
+ * Return: 1 sending the error to standard error
+ *
+ */
+int main(void)
+{
+	int sentence = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	s);
+	return(0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-README FILE FOR MY LOW LEVEL PROGRAMMING USING C  
+README FILE FOR MY LOW LEVEL PROGRAMMING USING C
+1. fundamentals; hello world, betty compliance 
+2. variables; control structures; loops  
