4a6e6759299416129f4075841d67b3d471296cd6
MarkTinega <martinozas@gmail.com>
2022-09-22 05:31:38 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,25 @@
 #include "main.h"
 
 /**
-  *_strncpy - 
-  *@dest:
-  *@src:
-  *@n:
+  *_strncpy - copy a string starting from index 0 of destination
+  *@dest: destination string
+  *@src: source string
+  *@n: number of characters to copy over
   *
-  *Return:
+  *Return: destination string that has been changed
   */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; n > i; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
