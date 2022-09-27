8d837de12037266c04ad4e8e5a79ca503bbe7fa8
MarkTinega <martinozas@gmail.com>
2022-09-27 12:32:40 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-  *_strstr - locate a substring 
+  *_strstr - locate a substring
   *@haystack: string
   *@needle: string substring
   *
@@ -12,27 +12,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, c;
-
-	i = 0;
-	c = 0;
-
-	while (haystack[i] != '\0')
+	while (*haystack != '\0')
 	{
-		j = 0;
-		while (needle[j + c] != '\0' && haystack[i + c] != '\0' 
-			&& needle[j + c] == haystack[i + c])
+		char *beginning = haystack;
+		char *pattern = needle;
+
+		while (*pattern == *haystack && *pattern != '\0'
+				&& *haystack != '\0')
 		{
-			if (haystack[i + c] != needle[j + c])
-				break;
-			c++;
+			haystack++;
+			pattern++;
 		}
-		if (needle[j + c] == '\0')
-			return (&haystack[i]);
-		j++;
-		i++
+		if (*pattern == '\0')
+			return (beginning);
+		haystack = beginning + 1;
 	}
 
-	return (NULL);
+	return ('\0');
 }
 
