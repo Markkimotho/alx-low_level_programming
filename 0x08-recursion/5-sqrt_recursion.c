4ed5caf71da9f8514f3b2ab4b4981eb7cd2369ce
MarkTinega <martinozas@gmail.com>
2022-09-28 12:14:00 -0700

    solutions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,12 +5,37 @@
   *
   *@n: integer to be evaluated
   *
-  *Return:
-  *
+  *Return: 0
   *
   */
 
 int _sqrt_recursion(int n)
 {
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (helper(n, 1));
+}
 
+/**
+  *help - helper function for sqrt_recursion()
+  *
+  *@n: number
+  *
+  *@i: incrementor
+  *Return: -1
+  */
+
+int help(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (helper(n, i + 1));
+	else
+		return (-1);
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,6 +10,24 @@
 
 int is_prime_number(int n)
 {
+	return (find_prime(n, 2, n / 2));
+}
 
-	is_prime_number(n)
+/**
+  *find_prime - checks for the actual prime
+  *@n: int
+  *@i: int
+  *@lim: where to stop
+  *
+  *Return: 0
+  */
+
+int find_prime(int n, int i, int lim)
+{
+	if ((n % i == 0 && i <= lim) || n < 0 || n == 1)
+		return (0);
+	else if (n % i != 0 && i <= lim)
+		return (helper_prime(n, i + 1, lim));
+	else
+		return (1);
 }
