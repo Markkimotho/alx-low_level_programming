be12135f7b5b75fdc29e449dcd1c24ce2d19b42a
MarkTinega <martinozas@gmail.com>
2022-10-06 14:00:26 -0700

    readding

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,18 +10,33 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, (nmemb * size));
+	return (ptr);
+}
+
+/**
+  *_memset - sets first n bytes of the memory area
+  *@s: array to set
+  *@b: what to set it to
+  *@n: first n bytes
+  *
+  *Return: s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
-		return (NULL);
-
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
-
-	return (p);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
