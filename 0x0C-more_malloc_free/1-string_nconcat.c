b871acf2cd4f6377c5e91745faf7ba9f309a47a5
MarkTinega <martinozas@gmail.com>
2022-10-06 13:47:01 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,44 +11,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	int strlen1, i, c;
+	char *ptr, *empt;
+	unsigned int i, k;
 
+	empt = "";
 	if (s1 == NULL)
-		s1 = "";
+		s1 = empt;
 	if (s2 == NULL)
-		s2 = "";
-
-	strlen1 = (unsigned int)_strlen(s1);
-	p = malloc((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
-	{
-		if (i < strlen1)
-			p[i] = s1[i];
-		else
-			p[i] = s2[c++];
-	}
-	p[i] = '\0';
-
-	return (p);
-}
-
-/**
-  *_strlen - length of a string
-  *@s: a string
-  *Return: length of a string
-  *
-  */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+		s2 = empt;
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-
-	return (i);
+	ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+		ptr[i] = s2[k];
+	ptr[i] = '\0';
+	return (ptr);
 }
