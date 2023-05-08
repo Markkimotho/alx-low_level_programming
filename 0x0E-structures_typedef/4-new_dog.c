0fe1a4f600c4ef81e12796f6850b6c81f032bd05
Markkimotho <martinozas@gmail.com>
2023-05-08 07:50:02 +0300

    Removing trash files

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+  *new_dog - function that creates a new dog
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: owner of the dog
+  *
+  *Return: pointer to new dog
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if *new_dog
+}
