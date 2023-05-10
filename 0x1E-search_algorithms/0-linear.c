dff2ffeeb792e58f42161d45638857b6957937c5
Markkimotho <martinozas@gmail.com>
2023-05-10 14:08:47 +0300

    Making corrections: Betty checking the 0-linear.c source code file and enhancing the 4-0 file and adding more empty files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 
-/** 
+/**
 * linear_search - performs a linear search
 * @array: array to be searched
 * @size: size of the array
@@ -13,12 +13,13 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
-	if(array == NULL)
+
+	if (array == NULL)
 		return (-1);
 	for (index = 0; index < size; index++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-		if(array[index] == value)
+		if (array[index] == value)
 		{
 			return (index);
 		}
