c9734dffbfb5795d681fb66df346cfe1ae2d8829
Markkimotho <martinozas@gmail.com>
2023-05-10 18:43:46 +0300

    Averages time complexities of jump searches

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+* jump_search - performs a jump search
+* @array: array to be searched
+* @size: size of the array
+* @value: value to be looked for
+*
+* Return: Index of the value in the array
+* 	  -1 if not found or if array is NULL
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+}
